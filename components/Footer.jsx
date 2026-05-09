c622657cbe79e97d97eddfafe4b2588a01e3b730
rameshswami4444-crypto <rameshswami4444@gmail.com>
2026-05-09 07:59:41 -0700

    fixed tailwind css properly

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="p-5 text-center text-white">
-      © 2026 Vinayak IT Solutions
+      © 2026-27 Vinayak IT Solutions
     </footer>
   );
 }
